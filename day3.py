965662566dbf42b67d6ba77a82ea5dc32a36fde3
haloplove <1374292755@qq.com>
2024-05-27 11:28:49 +0800

    1

--- day3.py
+++ day3.py
@@ -1,3 +1,17 @@
 ﻿import torch
-print(torch.__version__)
-print(torch.cuda.is_available())
+
+#信息查看
+# print(torch.__version__)
+# print(torch.cuda.is_available())
+#print(help(torch.cuda.is_available))
+
+#张量练习
+z=torch.zeros(2,5,dtype=torch.int32)
+print(z)
+print(z.dtype)#创建指定张量
+
+torch.manual_seed(23)
+p=torch.rand(3,3)
+print(p)
+print(p.dtype)#创建随机张量
+
