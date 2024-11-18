981b3e965969c1d06db96ef388b9140dc2604630
haloplove <1374292755@qq.com>
2024-11-18 20:41:30 +0800

    更新模型下载路径，修改模型ID，并在笔记本中添加模型加载和测试代码

--- Rag_finetune/download.py
+++ Rag_finetune/download.py
@@ -1,6 +1,7 @@
 from modelscope.hub.snapshot_download import snapshot_download
 
-model_id = "qwen/qwen2.5-7b"
-download_path = "/root/model"  # 替换为你想要的下载路径
+# model_id = "qwen/qwen2.5-7b"
+model_id = "qwen/Qwen2.5-0.5B-Instruct"
+download_path = "C:/Users/Admin/Desktop/model"  # 替换为你想要的下载路径
 
 snapshot_download(model_id, cache_dir=download_path)
